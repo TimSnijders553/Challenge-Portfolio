593edabda729da71f68ea427e231d9fed06b123b
Tim Snijders <tim.snijders@student.rocva.nl>
2020-12-15 11:42:17 +0100

    portfolio

---
+++ website/about me/script.js
@@ -0,0 +1,12 @@
+var bezoeker = {
+    naam : prompt("Wat is je naam?"),
+    leeftijd : prompt("Wat is je leeftijd?"),
+};
+if (bezoeker.leeftijd <= 18) {
+    document.body.style.backgroundColor = "red";
+} else if (bezoeker.leeftijd > 18) {
+    document.body.style.backgroundColor = "green";
+}
+
+
+      document.querySelector("#headtext").innerHTML = "Welkom " + (bezoeker.naam);
